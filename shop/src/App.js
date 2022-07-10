b05fe96a8e9a8a628ed7aa06fe5f09577e00d09c
yuity0725 <cherryblossoms0725@gmail.com>
2022-07-10 18:45:37 +0900

    フロント見た目だけ

---
+++ shop/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Col, Container, Row, Table} from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
+
+function RenderForm() {
+  return (
+    <>
+      <h2>Add items</h2>
+      <Form className>
+        <Form.Group controlId="formBasicId">
+          <Form.Label>id</Form.Label>
+          <Form.Control type="email" placeholder="Enter id"/>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicQuantity">
+          <Form.Label>Quantity</Form.Label>
+          <Form.Control type="password" placeholder="Enter quantity"/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stock List</h1>
+      <Container>
+        <Row>
+          <Col xs={8}>
+            <h2>Stocl List Table</h2>
+            <Table>
+              <thead>
+                <tr>
+                  <td>item_id</td>
+                  <td>name</td>
+                  <td>quantity</td>
+                </tr>
+              </thead>
+            </Table>
+          </Col>
+          <Col xs={4}>
+            <RenderForm/>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
